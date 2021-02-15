4960bab158937478f469fd6eae3f46ce9423f838
Jason Lyle <jblyle@clemson.edu>
2021-02-15 22:30:33 +0000

    Updated 7 files under /users

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Driver, GenericUser, Sponsor, Admin
 
-# Register your models here.
+class AdminDriver(admin.ModelAdmin):
+	model = Driver
+	list_display = ('username', 'first_name', 'last_name', 'phone_num', 'email', 'address', 'points')
+
+class AdminGenericUser(admin.ModelAdmin):
+	model = GenericUser
+	list_display = ('username', 'type')
+
+class AdminSponsor(admin.ModelAdmin):
+	model = Sponsor
+	list_display = ('username', 'first_name', 'last_name', 'email', 'sponsor_company')
+
+class AdminAdmin(admin.ModelAdmin):
+	model = Admin
+	list_display = ('username', 'first_name', 'last_name', 'email')
+
+admin.site.register(Driver, AdminDriver)
+admin.site.register(GenericUser, AdminGenericUser)
+admin.site.register(Sponsor, AdminSponsor)
+admin.site.register(Admin, AdminAdmin)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+
+# Model for the User Table - all users will be stored in here and this table will be checked on login
+class GenericUser(models.Model):
+    username = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
+    type = models.CharField(max_length=15)
+
+
+# Model for Driver Table - this is missing some fields, will have to decide how to fill out later
+class Driver(models.Model):
+    username = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=30)
+    phone_num = models.CharField(max_length=15)
+    email = models.CharField(max_length=30)
+    address = models.CharField(max_length=50)
+    points = models.IntegerField(default=0)
+
+
+class Sponsor(models.Model):
+    username = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=30)
+    email = models.CharField(max_length=30)
+    sponsor_company = models.CharField(max_length=30)
+# driver list
+
+
+class Admin(models.Model):
+    username = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=30)
+    email = models.CharField(max_length=30)
